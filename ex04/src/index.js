f52988d6d4d0b0a4ab623d4c4c3879d370042233
stefan <stefansgit@gmail.com>
2021-03-27 01:21:50 +0100

    Destructuring

---
+++ ex04/src/index.js
@@ -0,0 +1,18 @@
+// src.index.js
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+// add the following code below this line
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello, React!</h1>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
