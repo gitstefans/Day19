d052888c8bcbbd36dc4a035a9bafa1436cb12dfa
stefan <stefansgit@gmail.com>
2021-03-27 01:32:10 +0100

    JSX

--- ex06/src/index.js
+++ ex06/src/index.js
@@ -10,7 +10,7 @@ class App extends React.Component {
     // Change code above this line
     return (
       <div className="App">
-        <h1>Hello, { world }!</h1>   
+        <h1>Hello, {world}!</h1>   
       </div>
     );
   }
